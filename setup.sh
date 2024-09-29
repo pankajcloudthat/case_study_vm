47524348b6bcb70fdea1bc3088982d782366c025
pankajcloudthat <93825810+pankajcloudthat@users.noreply.github.com>
2024-09-29 17:17:21 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,7 +13,7 @@ mv kafka_2.13-3.4.0 kafka
 rm kafka_2.13-3.4.0.tgz
 
 # Set Kafka path in environment variables
-echo "-------------------------------------------10"
+echo "-------------------------------------------"
 sleep 10
 cdir=$(pwd)
 echo "export PATH=\$PATH:$cdir/kafka/bin" >> ~/.bashrc
@@ -28,19 +28,13 @@ sleep 2
 mip=$(curl ifconfig.me)
 echo "advertised.listeners=PLAINTEXT://$mip:9092" >> kafka/config/server.properties
 
-echo "-------------------------------------------"
-sleep 2
-echo "Currect IP: $mip"
-echo "advertised.listeners=PLAINTEXT://$mip:9092"
-
-
 # Install MySQL server
 echo "-------------------------------------------"
 sleep 2
 sudo apt install mysql-server -y
 
 # Configure MySQL to allow connections from any IP
-echo "-------------------------------------------2"
+echo "-------------------------------------------"
 echo "Update MySQL IP Binding"
 sleep 2
 sudo sed -i 's/^bind-address.*/bind-address = 0.0.0.0/' /etc/mysql/mysql.conf.d/mysqld.cnf
@@ -50,26 +44,12 @@ sudo systemctl restart mysql
 
 # MySQL configuration
 
-sleep 10
 echo "------------------------------------------"
 echo "|          Configure MySQL               |"
 echo "------------------------------------------"
+sleep 10
 
 sudo mysql -e "CREATE DATABASE IF NOT EXISTS stockdb;
 CREATE USER IF NOT EXISTS 'stock'@'%' IDENTIFIED BY 'stock';
 GRANT ALL PRIVILEGES ON stock.* TO 'stock'@'%';
 FLUSH PRIVILEGES;"
-
-# Show databases and users
-sudo mysql -e "SHOW DATABASES;"
-sudo mysql -e "SELECT User, Host FROM mysql.user;"
-
-
-echo "-------------------------------------------10"
-sleep 5
-# Start Zookeeper and Kafka server
-nohup zookeeper-server-start.sh kafka/config/zookeeper.properties > zookeeper.log 2>&1 &
-nohup kafka-server-start.sh kafka/config/server.properties > broker.log 2>&1 &
-
-# Check if the Kafka broker is running
-zookeeper-shell.sh localhost:2181 ls /brokers/ids
