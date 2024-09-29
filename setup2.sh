4a781a977690b12dc8752f7eeb3de02a9d2831ce
pankajcloudthat <93825810+pankajcloudthat@users.noreply.github.com>
2024-09-29 16:51:50 +0530

    Create setup2.sh

---
+++ setup2.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Update system packages
+sudo apt update
+
+# Install OpenJDK 8
+sudo apt install openjdk-8-jdk -y
+
+# Download and extract Kafka
+sleep 2
+wget https://archive.apache.org/dist/kafka/3.4.0/kafka_2.13-3.4.0.tgz
+tar xzf kafka_2.13-3.4.0.tgz
+mv kafka_2.13-3.4.0 kafka
+rm kafka_2.13-3.4.0.tgz
+
+# Set Kafka path in environment variables
+sleep 2
+cdir=$(pwd)
+echo "export PATH=\$PATH:$cdir/kafka/bin" >> ~/.bashrc
+
+sleep 2
+source ~/.bashrc
+
+# Configure Kafka's advertised listeners using the public IP
+sleep 2
+mip=$(curl ifconfig.me)
+echo "advertised.listeners=PLAINTEXT://$mip:9092" >> kafka/config/server.properties
+
+
+# Install MySQL server
+sleep 2
+sudo apt install mysql-server -y
+
+# Configure MySQL to allow connections from any IP
+sleep 2
+sudo sed -i 's/^bind-address.*/bind-address = 0.0.0.0/' /etc/mysql/mysql.conf.d/mysqld.cnf
+
+sleep 1
+sudo systemctl restart mysql
+
+# MySQL configuration
+sleep 10
+sudo mysql -e "CREATE DATABASE IF NOT EXISTS stockdb;
+CREATE USER IF NOT EXISTS 'stock'@'%' IDENTIFIED BY 'stock';
+GRANT ALL PRIVILEGES ON stockdb.* TO 'stock'@'%';
+FLUSH PRIVILEGES;"
