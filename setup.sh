8e0ee439fd8d1712a3249e3da138aa025e0eb177
pankajcloudthat <93825810+pankajcloudthat@users.noreply.github.com>
2024-09-29 15:57:12 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,29 +13,38 @@ mv kafka_2.13-3.4.0 kafka
 rm kafka_2.13-3.4.0.tgz
 
 # Set Kafka path in environment variables
+echo "-------------------------------------------10"
+sleep 10
 cdir=$(pwd)
 echo "export PATH=\$PATH:$cdir/kafka/bin" >> ~/.bashrc
 source ~/.bashrc
 
 # Configure Kafka's advertised listeners using the public IP
+echo "-------------------------------------------02"
+sleep 2
 mip=$(curl ifconfig.me)
 echo "advertised.listeners=PLAINTEXT://$mip:9092" >> kafka/config/server.properties
 
+echo "-------------------------------------------01"
+sleep 1
+echo "Currect IP: $mip"
+echo "advertised.listeners=PLAINTEXT://$mip:9092"
+
+
 # Install MySQL server
+echo "-------------------------------------------01"
+sleep 1
 sudo apt install mysql-server -y
 
 # Configure MySQL to allow connections from any IP
+echo "-------------------------------------------10"
+sleep 10
 sudo sed -i 's/^bind-address.*/bind-address = 0.0.0.0/' /etc/mysql/mysql.conf.d/mysqld.cnf
 sudo systemctl restart mysql
 
-# Start Zookeeper and Kafka server
-nohup zookeeper-server-start.sh kafka/config/zookeeper.properties > zookeeper.log 2>&1 &
-nohup kafka-server-start.sh kafka/config/server.properties > broker.log 2>&1 &
-
-# Check if the Kafka broker is running
-zookeeper-shell.sh localhost:2181 ls /brokers/ids
-
 # MySQL configuration
+echo "-------------------------------------------10"
+sleep 10
 echo "------------------------------------------"
 echo "|          Configure MySQL               |"
 echo "------------------------------------------"
@@ -48,3 +57,13 @@ FLUSH PRIVILEGES;"
 # Show databases and users
 sudo mysql -e "SHOW DATABASES;"
 sudo mysql -e "SELECT User, Host FROM mysql.user;"
+
+
+echo "-------------------------------------------10"
+sleep 5
+# Start Zookeeper and Kafka server
+nohup zookeeper-server-start.sh kafka/config/zookeeper.properties > zookeeper.log 2>&1 &
+nohup kafka-server-start.sh kafka/config/server.properties > broker.log 2>&1 &
+
+# Check if the Kafka broker is running
+zookeeper-shell.sh localhost:2181 ls /brokers/ids
